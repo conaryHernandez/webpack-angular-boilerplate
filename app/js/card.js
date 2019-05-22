2e14b3899d66f1a2e04ba025ffc13f867abbc8e0
conaryh <conaryh@gmail.com>
2019-05-22 14:56:25 -0600

    setup webpack for bigger apps and updgrade webpack 2 to 4

---
+++ app/js/card.js
@@ -0,0 +1,10 @@
+﻿class Card {
+    constructor(color, suit, value, name) {
+        this.color = color;
+        this.suit = suit;
+        this.value = value;
+        this.name = name;
+    }
+}
+
+export { Card };
---
+++ app/js/game.js
@@ -0,0 +1,31 @@
+﻿import {Deck} from './deck';
+import {Player} from './player';
+
+class Game {
+    constructor(playerNames, acesHigh) {
+        let cards = new Deck(acesHigh);
+
+        this.players = [];
+        this.players.push(new Player('Dealer'));
+        this.cards = Deck.shuffle(cards);
+
+        playerNames.forEach(function (name) {
+            this.players.push(new Player(name));
+        }, this);
+
+    }
+
+    start() {
+        Deck.deal(this.players, 2, this.cards);
+    }
+
+    dealerTurn() {
+        Deck.deal([this.players[0]], 1, this.cards);
+    }
+
+    playerTurn() {
+        Deck.deal([this.players[1]], 1, this.cards);
+    }
+}
+
+export { Game };
